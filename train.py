f9145168b2bf70a6c226259d8d7ff704b1f751bd
Azoffsky <azoffsky@yahoo.com>
2022-09-03 16:07:22 +0300

    train
    training network

---
+++ train.py
@@ -0,0 +1,158 @@
+# USAGE
+
+# python train.py --model output/model.pth --plot output/plot.png
+
+import matplotlib
+matplotlib.use("Agg")
+
+from model_cnn import Net
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.optim import SGD
+from torch import nn
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import torch
+import time
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", type=str, required=True,
+                help="path to output trained model")
+ap.add_argument("-p", "--plot", type=str, required=True,
+                help="path to output loss/accuracy plot")
+args = vars(ap.parse_args())
+
+INIT_LR = 1e-2
+BATCH_SIZE = 64
+EPOCHS = 10
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_transform = transforms.Compose([
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+valid_transform = transforms.Compose([
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+train_dataset = datasets.ImageFolder(
+    root='train',
+    transform=train_transform
+)
+
+valid_dataset = datasets.ImageFolder(
+    root='valid',
+    transform=valid_transform
+)
+
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, shuffle=True,
+    num_workers=0, pin_memory=True
+)
+
+valid_loader = DataLoader(
+    valid_dataset, batch_size=BATCH_SIZE, shuffle=False,
+    num_workers=0, pin_memory=True
+)
+
+trainSteps = len(train_loader.dataset) // BATCH_SIZE
+valSteps = len(valid_loader.dataset) // BATCH_SIZE
+
+print("[INFO] initializing the network model...")
+model = Net(numChannels=3, classes=3)
+
+opt = SGD(model.parameters(), lr=INIT_LR)
+lossFn = nn.NLLLoss()
+
+H = {
+    "train_loss": [],
+    "train_acc": [],
+    "val_loss": [],
+    "val_acc": []
+}
+
+print("[INFO] training the network...")
+startTime = time.time()
+
+for e in range(0, EPOCHS):
+    model.train()
+
+    totalTrainLoss = 0
+    totalValLoss = 0
+
+    trainCorrect = 0
+    valCorrect = 0
+
+    for (x, y) in train_loader:
+        (x, y) = (x.to(device), y.to(device))
+
+        pred = model(x)
+        loss = lossFn(pred, y)
+
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+
+        totalTrainLoss += loss
+        trainCorrect += (pred.argmax(1) == y).type(
+            torch.float).sum().item()
+
+    with torch.no_grad():
+        model.eval()
+
+        for (x, y) in valid_loader:
+            (x, y) = (x.to(device), y.to(device))
+
+            pred = model(x)
+            totalValLoss += lossFn(pred, y)
+
+            valCorrect += (pred.argmax(1) == y).type(
+                torch.float).sum().item()
+
+    avgTrainLoss = totalTrainLoss / trainSteps
+    avgValLoss = totalValLoss / valSteps
+
+    trainCorrect = trainCorrect / len(train_loader.dataset)
+    valCorrect = valCorrect / len(valid_loader.dataset)
+
+    H["train_loss"].append(avgTrainLoss.cpu().detach().numpy())
+    H["train_acc"].append(trainCorrect)
+    H["val_loss"].append(avgValLoss.cpu().detach().numpy())
+    H["val_acc"].append(valCorrect)
+
+    print("[INFO] EPOCH: {}/{}".format(e + 1, EPOCHS))
+    print("Train loss: {:.6f}, Train accuracy: {:.4f}".format(
+        avgTrainLoss, trainCorrect))
+    print("Val loss: {:.6f}, Val accuracy: {:.4f}\n".format(
+        avgValLoss, valCorrect))
+
+endTime = time.time()
+print("[INFO] total time taken to train the model: {:.2f}s".format(
+    endTime - startTime))
+
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(H["train_loss"], label="train_loss")
+plt.plot(H["val_loss"], label="val_loss")
+plt.plot(H["train_acc"], label="train_acc")
+plt.plot(H["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.savefig(args["plot"])
+
+torch.save(model, args["model"])
